5f1057e3fab7bb4ddee1aeea766a441de989a0ab
H570-PLUS\masashi <hatano153@gmail.com>
2022-01-15 19:39:03 +0900

    Update

---
+++ ctrans/ctrans_inv.py
@@ -0,0 +1,57 @@
+import sys
+from pathlib import Path
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from ctrans import get_data_from_csv
+
+def worldToCamera(real_coordinate, R, T):
+    real_coordinate_camera = np.dot(R.T, (real_coordinate-T))
+    return real_coordinate_camera
+
+def cameraToScreen(real_coordinate_camera, image, K):
+    ox = K[0,2]
+    oy = K[1,2]
+    fx = K[0,0]
+    fy = K[1,1]
+
+    u = (fy*real_coordinate_camera[1][0]/abs(real_coordinate_camera[2][0]) + (image.shape[1]-oy))
+    v = (fx*real_coordinate_camera[0][0]/abs(real_coordinate_camera[2][0]) + ox)
+
+    coordinates = (int(u),int(v))
+    return coordinates
+
+def plot(image, coordinates):
+    for i in range(len(coordinates)):
+        cv2.circle(image, coordinates[i], 5, (255,0,0), -1)
+    image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('images/result.jpg', image_bgr)
+
+def main():
+    time = []
+    with open('timestamp.txt') as f:
+        for line in f:
+            time.append(line.strip())
+    
+    predictions = []
+    sys.path.append(str(Path('coordinate_transformation_inv.py').resolve().parent.parent))
+    with open(sys.path[-1]+'\\socialgan\\datasets\\original\\scean1\\output.txt') as f:
+        for line in f:
+            predictions.append(line.strip().split('\t'))
+
+    R, K, T = get_data_from_csv('1207_1444_12.csv', int(time[-1]))
+    image_path = 'images/'+time[-1]+'.jpg'
+    im = plt.imread(image_path)
+    image = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
+
+    coordinates = []
+    for i in range(len(predictions)):
+        real_coordinate = np.array([float(predictions[i][2]),-1.35,float(predictions[i][3])]).reshape(3,1)
+        real_coordinate_camera = worldToCamera(real_coordinate, R, T)
+        coordinates.append(cameraToScreen(real_coordinate_camera, image, K))
+    
+    plot(image, coordinates)
+    
+if __name__ == '__main__':
+    main()
---
+++ ctrans/human_detection.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_foot_coordinates(path_image):
+    im = plt.imread(path_image)
+    image = cv2.rotate(im, cv2.ROTATE_90_CLOCKWISE)
+    res = cv2.resize(image, dsize=(416, 416), interpolation=cv2.INTER_CUBIC)
+
+    classes = []
+    with open('coco.names', 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
+
+
+    Width = res.shape[1]
+    Height = res.shape[0]
+
+    # read pre-trained model and config file
+    net = cv2.dnn.readNet('yolov3/yolov3.weights', 'yolov3/yolov3.cfg')
+
+    # create input blob 
+    # set input blob for the network
+    net.setInput(cv2.dnn.blobFromImage(res, 0.00392, (416,416), (0,0,0), True, crop=False))
+
+    # run inference through the network
+    # and gather predictions from output layers
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    outs = net.forward(output_layers)
+
+
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    # create bounding box 
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.1:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.1, 0.1)
+
+    coordinates = []
+    # get detected person's foot coordinate
+    for i in indices:
+        box = boxes[i]
+        if class_ids[i]==0:
+            label = str(classes[class_id])
+            coordinate_x = round((box[0]+box[2]/2)*image.shape[1]/416)
+            coordinate_y = round((box[1]+box[3])*image.shape[0]/416)
+            coordinates.append((coordinate_x,coordinate_y))
+            cv2.rectangle(res, (round(box[0]),round(box[1])), (round(box[0]+box[2]),round(box[1]+box[3])), (255, 0, 0), 1)
+            cv2.putText(res, label, (round(box[0])-10,round(box[1])-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+
+    # resize resized image to the original scale
+    result = cv2.resize(res, dsize=(image.shape[1], image.shape[0]), interpolation=cv2.INTER_CUBIC)
+
+    plt.imshow(result)
+    plt.show()
+
+    return coordinates, image
